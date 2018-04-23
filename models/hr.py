db27d61d43f542d782909cfd87627b0bbc66ef6a
Jefferson Silva <jeffer.leo93@gmail.com>
2018-04-23 11:09:07 -0600

    Campos de edad y ordenar en nuevas pestañas campos existentes

--- models/hr.py
+++ models/hr.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import datetime
 
 class hr_employee(models.Model):
     _inherit = 'hr.employee'
@@ -29,4 +29,38 @@ class hr_employee(models.Model):
     jornada_trabajo = fields.Char('Jornada de Trabajo')
     permiso_trabajo = fields.Char('Permiso de Trabajo')
     contacto_emergencia = fields.Many2one('res.partner','Contacto de Emergencia')
+    marital = fields.Selection([
+        ('single', 'Single'),
+        ('married', 'Married'),
+        ('widower', 'Widower'),
+        ('divorced', 'Divorced'),
+        ('separado', 'Separado(a)'),
+        ('unido', 'Unido(a)')
+    ], string='Marital Status', groups='hr.group_hr_user')
+    edad = fields.Integer(compute='_get_edad',string='Edad')
+    vecindad_dpi = fields.Char('Vecindad DPI')
+    tarjeta_salud = fields.Boolean('Tarjeta de salud')
+    tarjeta_manipulacion = fields.Boolean('Tarjeta de manipulación')
+    tarjeta_pulmones = fields.Boolean('Tarjeta de pulmones')
 
+    def _get_edad(self):
+        for employee in self:
+            diaNacimiento = int(datetime.datetime.strptime(str(employee.birthday),'%Y-%m-%d').date().strftime('%d'))
+            mesNacimiento = int(datetime.datetime.strptime(str(employee.birthday),'%Y-%m-%d').date().strftime('%m'))
+            anioNacimiento = int(datetime.datetime.strptime(str(employee.birthday),'%Y-%m-%d').date().strftime('%Y'))
+            diaActual = int(datetime.date.today().strftime('%d'))
+            mesActual = int(datetime.date.today().strftime('%m'))
+            anioActual = int(datetime.date.today().strftime('%Y'))
+            
+            restaDia = diaActual - diaNacimiento
+            restaMes = mesActual - mesNacimiento
+            restaAnio = anioActual - anioNacimiento
+            
+            if (restaMes < 0):
+                restaAnio = restaAnio -1
+            elif (restaMes == 0):
+                if (restaDia < 0):
+                    restaAnio = restaAnio - 1
+                if (restaDia > 0):
+                    restaAnio = restaAnio
+            employee.edad = restaAnio
