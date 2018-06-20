bc5bd53f7469904bc57c0462eed102e36db6d9e9
Jefferson Silva <jeffer.leo93@gmail.com>
2018-06-20 10:28:29 -0600

    Separar por cuentas analiticas la planilla

--- models/hr_payroll.py
+++ models/hr_payroll.py
@@ -7,8 +7,4 @@ class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
     dia_del_mes = fields.Integer('Dia del Mes')
-
-class HrPayslipWorkedDays(models.Model):
-    _inherit = 'hr.payslip.worked_days'
-
-    dias_totales_mes = fields.Float('Dias totales')
+    dias_totales_mes = fields.Integer('Dias totales') 
