9a4585533b439ae2c0ca9cfe90a0bbf856146202
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2019-06-06 03:31:35 +0000

    Poder cerrar nominas

--- __manifest__.py
+++ __manifest__.py
@@ -31,6 +31,7 @@
         'wizard/rrhh_libro_salarios_view.xml',
         'wizard/rrhh_informe_empleador_view.xml',
         'wizard/igss.xml',
+        'wizard/cerrar_nominas.xml',
         'security/ir.model.access.csv',
     ],
 }
--- wizard/__init__.py
+++ wizard/__init__.py
@@ -4,3 +4,4 @@ from . import planilla
 from . import rrhh_libro_salarios
 from . import rrhh_informe_empleador
 from . import igss
+from . import cerrar_nominas
