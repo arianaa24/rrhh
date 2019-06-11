536e09ecda2cc757261bfff2e82ec3078d37fb27
Jefferson Silva <jeffer.leo93@gmail.com>
2019-06-11 09:04:03 -0600

    Dias trabajados

--- models/hr_payroll.py
+++ models/hr_payroll.py
@@ -50,7 +50,7 @@ class HrPayslip(models.Model):
                 if dias.code in tipos_ausencias['ausencias_restar']:
                     dias_ausentados_restar += dias.number_of_days
             if nomina.date_from <= nomina.employee_id.contract_id.date_start <= nomina.date_to:
-                dias_laborados = nomina.employee_id.get_work_days_data(Datetime.from_string(str(nomina.employee_id.contract_id.date_start())), Datetime.from_string(str(nomina.date_to)), calendar=nomina.employee_id.contract_id.resource_calendar_id)
+                dias_laborados = nomina.employee_id.get_work_days_data(Datetime.from_string(nomina.employee_id.contract_id.date_start), Datetime.from_string(nomina.date_to), calendar=nomina.employee_id.contract_id.resource_calendar_id)
                 nomina.worked_days_line_ids = [(0, 0, {'name': 'Dias trabajados', 'code': 'TRABAJO100', 'number_of_days': (dias_laborados['days'] + dias_ausentados_sumar), 'contract_id': nomina.employee_id.contract_id.id})]
             else:
                 if nomina.employee_id.contract_id.schedule_pay == 'monthly':
