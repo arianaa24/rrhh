f74adc823c0377a4da43b193d50d6814013699b9
rocketgithub <rodrigo@solucionesprisma.com>
2019-06-13 09:18:11 -0600

    Update hr_payroll.py

--- models/hr_payroll.py
+++ models/hr_payroll.py
@@ -144,12 +144,12 @@ class HrPayslip(models.Model):
             dias_laborados = self.employee_id.get_work_days_data(Datetime.from_string(contracts.date_start), Datetime.from_string(date_to), calendar=contracts.resource_calendar_id)
             logging.warn(dias_laborados)
             logging.warn(dias_ausentados_sumar)
-            res.append({'name': 'Dias trabajados', 'sequence': 10,'code': 'TRABAJO100', 'number_of_days': (dias_laborados['days'] - dias_ausentados_restar), 'contract_id': contracts.id})
+            res.append({'name': 'Dias trabajados', 'sequence': 10,'code': 'TRABAJO100', 'number_of_days': (dias_laborados['days'] + dias_ausentados_sumar), 'contract_id': contracts.id})
         else:
             if contracts.schedule_pay == 'monthly':
-                res.append({'name': 'Dias trabajados','sequence': 10,'code': 'TRABAJO100','number_of_days': 30 - dias_ausentados_restar, 'contract_id': contracts.id})
+                res.append({'name': 'Dias trabajados','sequence': 10,'code': 'TRABAJO100','number_of_days': 30 + dias_ausentados_sumar, 'contract_id': contracts.id})
             if contracts.schedule_pay == 'bi-monthly':
-                res.append({'name': 'Dias trabajados','sequence': 10,'code': 'TRABAJO100','number_of_days': 15 - dias_ausentados_restar, 'contract_id': contracts.id})
+                res.append({'name': 'Dias trabajados','sequence': 10,'code': 'TRABAJO100','number_of_days': 15 + dias_ausentados_sumar, 'contract_id': contracts.id})
         return res
 
 class HrPayslipRun(models.Model):
