db49a6a4295490a2ab22b999521b68302bb7639f
Ariana Carrillo <30884211+arianaa24@users.noreply.github.com>
2020-03-25 10:40:22 -0600

    Cambios en reporte planilla.

--- wizard/planilla_pdf.py
+++ wizard/planilla_pdf.py
@@ -19,7 +19,11 @@ class report_planilla_pdf(models.AbstractModel):
         reporte['puestos'] = {}
         reporte['lineas'] = []
         reporte['suma'] = {}
-        reporte['total'] = 0
+        reporte['total'] = {}
+        reporte['total'] = []
+        totals1 = [0 for c in planilla.columna_id]
+        totals1.append(0)
+        reporte['total'] = totals1
         
         columnas = []
         for columna in planilla.columna_id:
@@ -34,6 +38,7 @@ class report_planilla_pdf(models.AbstractModel):
                 llave = slip.contract_id.analytic_account_id.name
             else:
                 llave = 'Indefinido'
+            
             
             if llave not in lineas:
                 lineas[llave] = {}
@@ -51,7 +56,10 @@ class report_planilla_pdf(models.AbstractModel):
 
             if llave not in reporte['cuentas_analiticas']:
                 reporte['cuentas_analiticas'].append(llave)
-                reporte['suma'][llave] = 0
+                reporte['suma'][llave] = []
+                totals = [0 for c in planilla.columna_id]
+                totals.append(0)
+                reporte['suma'][llave] = totals
 
             if llave not in reporte['puestos']:
                 reporte['puestos'][llave] = []
@@ -100,17 +108,18 @@ class report_planilla_pdf(models.AbstractModel):
                 if c.sumar:
                     total_salario += total_columna
                     
-                    
 
                 linea['dinamico'].append(total_columna)
                 lineas[llave][slip.employee_id.job_id.name]['totales'][x] += total_columna
+                reporte['suma'][llave][x] += total_columna
+                reporte['total'][x] += total_columna
 #                totales[x] += total_columna
                 x += 1
             
             linea['dinamico'].append(total_salario)
             lineas[llave][slip.employee_id.job_id.name]['totales'][len(totales) - 1] += total_salario
-            reporte['suma'][llave] += total_salario
-            reporte['total'] += total_salario
+            reporte['suma'][llave][len(totals) - 1] += total_salario
+            reporte['total'][len(totals) - 1] += total_salario
             
 #            totales[len(totales) - 1] += total_salario
             linea['estatico']['banco_depositar'] = slip.employee_id.bank_account_id.bank_id.name
